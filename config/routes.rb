18d362cc6680b92bba51068045cd88a203dcac23
Alex Filippov <alex.filippov@profitero.com>
2021-04-15 00:01:34 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'sessions/create'
   get 'sessions/destroy'
-  get 'pages/home'
+  root 'pages#home'
   get 'pages/account'
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
